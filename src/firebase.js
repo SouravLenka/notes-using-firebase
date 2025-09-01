89803848a6658abb840b87755dfc5b6704768b32
Souravlenka <lenkasourav09@gmail.com>
2025-09-01 11:51:18 +0530

    3

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBmxMWwwIYDM4cTJiHSapMaHYFAOWinZAI",
-  authDomain: "notes-93b17.firebaseapp.com",
-  projectId: "notes-93b17",
-  storageBucket: "notes-93b17.firebasestorage.app",
-  messagingSenderId: "502400754721",
-  appId: "1:502400754721:web:5d92797c008b45db5daf60",
-  measurementId: "G-0ELEND6Z9S"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmxMWwwIYDM4cTJiHSapMaHYFAOWinZAI",
+    authDomain: "notes-93b17.firebaseapp.com",
+    projectId: "notes-93b17",
+    storageBucket: "notes-93b17.firebasestorage.app",
+    messagingSenderId: "502400754721",
+    appId: "1:502400754721:web:5d92797c008b45db5daf60",
+    measurementId: "G-0ELEND6Z9S"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
